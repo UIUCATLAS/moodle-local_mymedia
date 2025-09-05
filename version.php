181d05500ef81662dd71a5734505e30bc4a304ad
Gosha Nikiforov <gosha@illinois.edu>
2025-09-05 11:27:54 -0500

    update t- MOODLE_405_STABLE

--- version.php
+++ version.php
@@ -25,11 +25,11 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-$plugin->version = 2022041904;
+$plugin->version = 2024100702;
 $plugin->component = 'local_mymedia';
-$plugin->release = 'Kaltura release 4.4.3';
-$plugin->requires = 2022041902;
+$plugin->release = 'Kaltura release 4.5.1';
+$plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'local_kaltura' => 2022041904
+    'local_kaltura' => 2024100702
 );
